d1cfe96a599edbf81f5740d41f76641a8e1b697a
nicolas mejia <nicolasmejiaco@gmail.com>
2015-03-23 15:40:01 -0500

    Clase 02 de Programacion Web. Css y BootstrapClase 02 de Programacion Web. Css y Bootstrap

---
+++ app/controllers/Clase2Controller.php
@@ -0,0 +1,9 @@
+<?php
+
+class Clase2Controller extends BaseController {
+
+    public function getCss() {
+        return View::make('index');
+    }
+
+}
---
+++ app/views/index.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <title>Bienvenido a mi pagina web</title>
+        <link rel="stylesheet" href="../../public/assets/css/style.css">
+    </head>
+    <body>
+        <h1 class="titulo">Bienvenido a mi pagina web</h1>
+        <h1 id="logo">MI LOGO</h1>
+
+        <h1 class="z">Titulo 1</h1>
+
+        <h1 class="titulo">Titulo 2</h1>
+
+        <h1 class="z">Titulo 3</h1>
+
+        <div>
+            <p>Parrafo 1</p>
+            <p>Parrafo 2</p>
+            <div>
+                <span>
+                    <p>Parrafo 4</p>
+                </span>
+                <span>Span 2</span>
+            </div>
+        </div>
+
+        <p>Parrafo 3</p>
+
+        <ol>
+            <li>Nombre: Nicolas</li>
+            <li>Nacionalidad: Colombiana</li>
+            <li>Edad: 34</li>
+            <li>Ciudad de nacimiento: Manizales</li>
+            <li>Equipo Favorito: Once</li>
+            <li>Banda Favorita: STP</li>
+        </ol>
+    </body>
+</html>
